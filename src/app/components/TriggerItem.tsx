fdd493ecd058a0054b6f4694f6969baf79adf28b
Henri Chabert <chabhenrib@gmail.com>
2025-07-15 20:22:53 +0200

    Users

--- src/app/components/TriggerItem.tsx
+++ src/app/components/TriggerItem.tsx
@@ -1,5 +1,9 @@
 import browser from "webextension-polyfill";
-import { Trigger, TriggerEventMessage, TriggerEventMessageResponse } from "@/types/messages.type";
+import type {
+	Trigger,
+	TriggerEventMessage,
+	TriggerEventMessageResponse,
+} from "@/types/messages.type";
 import { Flex, Text, Badge, Button } from "@radix-ui/themes";
 import n8nIcon from "@/assets/n8n-icon.png";
 import zapierIcon from "@/assets/zapier-icon.png";
@@ -10,80 +14,91 @@ import { useState } from "react";
 import { useToaster } from "@/app/hooks/use-toaster";
 
 interface TriggerItemProps {
-  trigger: Trigger;
+	trigger: Trigger;
 }
 
 const icons = {
-  n8n: n8nIcon,
-  zapier: zapierIcon,
-  make: makeIcon,
+	n8n: n8nIcon,
+	zapier: zapierIcon,
+	make: makeIcon,
 } as Record<Trigger["source"], string>;
 
 function TriggerItem({ trigger }: TriggerItemProps) {
-  const [loading, setLoading] = useState(false);
-  const { showToast } = useToaster();
+	const [loading, setLoading] = useState(false);
+	const { showToast } = useToaster();
 
-  const getCurrentUrl = async () => {
-    const query = { active: true, currentWindow: true };
-    const [tab] = await browser.tabs.query(query);
-    return tab.url;
-  };
+	const getCurrentUrl = async () => {
+		const query = { active: true, currentWindow: true };
+		const [tab] = await browser.tabs.query(query);
+		return tab.url;
+	};
 
-  const onRun = async () => {
-    setLoading(true);
+	const onRun = async () => {
+		setLoading(true);
 
-    showToast({
-      description: "Running trigger",
-      variation: "success",
-    });
+		showToast({
+			description: "Running trigger",
+			variation: "success",
+		});
 
-    const currentUrl = await getCurrentUrl();
+		const currentUrl = await getCurrentUrl();
 
-    const response = (await browser.runtime.sendMessage({
-      type: "TRIGGER_EVENT",
-      payload: { event: { id: "manual_trigger_in_popup", context: { triggerId: trigger.id, url: currentUrl } } },
-    } as TriggerEventMessage)) as TriggerEventMessageResponse;
+		const response = (await browser.runtime.sendMessage({
+			type: "TRIGGER_EVENT",
+			payload: {
+				event: {
+					id: "manual_trigger_in_popup",
+					context: { triggerId: trigger.id, url: currentUrl },
+				},
+			},
+		} as TriggerEventMessage)) as TriggerEventMessageResponse;
 
-    if (response.success) {
-      showToast({
-        description: "Trigger run successfully",
-        variation: "success",
-      });
-    } else {
-      showToast({
-        description: "Failed to run trigger",
-        variation: "error",
-      });
-    }
+		if (response.success) {
+			showToast({
+				description: "Trigger run successfully",
+				variation: "success",
+			});
+		} else {
+			showToast({
+				description: "Failed to run trigger",
+				variation: "error",
+			});
+		}
 
-    setLoading(false);
-  };
+		setLoading(false);
+	};
 
-  return (
-    <Flex
-      justify="between"
-      align="center"
-      p="2"
-      width="100%"
-      className="border border-slate-200 rounded-md hover:bg-slate-200"
-    >
-      <Flex gap="2" align="center">
-        <img src={icons[trigger.source as keyof typeof icons]} width={16} height={16} className="h-4 w-4" />
-        <Flex direction="column" gap="1">
-          <Text size="2">{trigger.name}</Text>
-          <Flex gap="1">
-            <Badge size="1" radius="large">
-              {trigger.source}
-            </Badge>
-          </Flex>
-        </Flex>
-      </Flex>
-      <Button size="1" loading={loading} onClick={onRun}>
-        <PlayIcon size={16} />
-        <Text>Run</Text>
-      </Button>
-    </Flex>
-  );
+	return (
+		<Flex
+			justify="between"
+			align="center"
+			p="2"
+			width="100%"
+			className="border border-slate-200 rounded-md hover:bg-slate-200"
+		>
+			<Flex gap="2" align="center">
+				<img
+					src={icons[trigger.source as keyof typeof icons]}
+					width={16}
+					height={16}
+					className="h-4 w-4"
+					alt={trigger.source}
+				/>
+				<Flex direction="column" gap="1">
+					<Text size="2">{trigger.name}</Text>
+					<Flex gap="1">
+						<Badge size="1" radius="large">
+							{trigger.source}
+						</Badge>
+					</Flex>
+				</Flex>
+			</Flex>
+			<Button size="1" loading={loading} onClick={onRun}>
+				<PlayIcon size={16} />
+				<Text>Run</Text>
+			</Button>
+		</Flex>
+	);
 }
 
 export default TriggerItem;
--- src/extension/background.ts
+++ src/extension/background.ts
@@ -1,99 +1,115 @@
 import browser from "webextension-polyfill";
-import { Trigger } from "../services/hercule-server/hercule-api.types";
-import {
-  Message,
-  MessageResponse,
-  ErrorMessageResponse,
-  TriggerEventMessage,
-  TriggerEventMessageResponse,
-  LoginMessage
+import type { Trigger } from "../services/hercule-server/hercule-api.types";
+import type {
+	Message,
+	MessageResponse,
+	ErrorMessageResponse,
+	TriggerEventMessage,
+	TriggerEventMessageResponse,
+	LoginMessage,
 } from "../types/messages.type";
-import { ConnectMessage } from "../types/messages.type";
-import { ListTriggersMessageResponse } from "../types/messages.type";
+import type { ConnectMessage } from "../types/messages.type";
+import type { ListTriggersMessageResponse } from "../types/messages.type";
 import { triggerEventServices } from "@/services/trigger-event.service";
 import { herculeApiFromStorage } from "@/services/hercule-server/hercule-api";
-import { onConnectStatusMessage, onConnectMessage, onDisconnectMessage, onLoginMessage, onLogoutMessage } from "@/services/auth.service";
+import {
+	onConnectStatusMessage,
+	onConnectMessage,
+	onDisconnectMessage,
+	onLoginMessage,
+	onLogoutMessage,
+} from "@/services/auth.service";
 import { MessageTriggerEventService } from "@/services/trigger-event.service";
 import { getCurrentTabUrl } from "@/helpers/background-utils.helper";
 
 self.addEventListener("activate", () => {
-  registerEvents();
+	registerEvents();
 });
 
 const registerEvents = async () => {
-  browser.runtime.onMessage.addListener(onMessage);
-  triggerEventServices.forEach((service) => service.register());
+	browser.runtime.onMessage.addListener(onMessage);
+	for (const service of triggerEventServices) {
+		service.register();
+	}
 };
 
-const onTriggerEventMessage = async (message: TriggerEventMessage): Promise<TriggerEventMessageResponse> => {
-  const eventId = message.payload.event.id;
-  const service = MessageTriggerEventService.servicesMap[eventId];
+const onTriggerEventMessage = async (
+	message: TriggerEventMessage,
+): Promise<TriggerEventMessageResponse> => {
+	const eventId = message.payload.event.id;
+	const service = MessageTriggerEventService.servicesMap[eventId];
 
-  if (service) {
-    const shouldTrigger = await service.onEventShouldTrigger(message);
-    if (shouldTrigger) {
-      const response = await service.triggerEvent(message.payload.event.context);
-      return { success: response.success, payload: { response: response.payload } };
-    }
-  }
+	if (service) {
+		const shouldTrigger = await service.onEventShouldTrigger(message);
+		if (shouldTrigger) {
+			const response = await service.triggerEvent(
+				message.payload.event.context,
+			);
+			return {
+				success: response.success,
+				payload: { response: response.payload },
+			};
+		}
+	}
 
-  return { success: false };
+	return { success: false };
 };
 
-const onListTriggersMessage = async (): Promise<ListTriggersMessageResponse> => {
-  const herculeApi = await herculeApiFromStorage();
-  let triggers: Trigger[];
-  try {
-    const url = await getCurrentTabUrl();
-    triggers = await herculeApi.listTriggers({
-      event: "manual_trigger_in_popup",
-      url,
-    });
-  } catch (error: unknown) {
-    console.error("Error listing triggers:", error);
-    return { success: false, payload: { message: (error as Error).message } };
-  }
+const onListTriggersMessage =
+	async (): Promise<ListTriggersMessageResponse> => {
+		const herculeApi = await herculeApiFromStorage();
+		let triggers: Trigger[];
+		try {
+			const url = await getCurrentTabUrl();
+			triggers = await herculeApi.listTriggers({
+				event: "manual_trigger_in_popup",
+				url,
+			});
+		} catch (error: unknown) {
+			console.error("Error listing triggers:", error);
+			return { success: false, payload: { message: (error as Error).message } };
+		}
 
-  return {
-    success: true,
-    payload: {
-      triggers: triggers.map((trigger) => {
-        return {
-          id: trigger.id,
-          name: trigger.name,
-          source: trigger.source,
-          urlRegex: trigger.url_regex,
-        };
-      }),
-    },
-  };
-};
+		return {
+			success: true,
+			payload: {
+				triggers: triggers.map((trigger) => {
+					return {
+						id: trigger.id,
+						name: trigger.name,
+						source: trigger.source,
+						urlRegex: trigger.url_regex,
+					};
+				}),
+			},
+		};
+	};
 
 const onFallbackMessage = async (): Promise<ErrorMessageResponse> => {
-  return { success: false, payload: { message: "Invalid message type" } };
+	return { success: false, payload: { message: "Invalid message type" } };
 };
 
 const onMessage = async (message: unknown): Promise<MessageResponse> => {
-  const messageWithType = message as Message;
+	const messageWithType = message as Message;
 
-  console.debug("📨 Received message:", messageWithType);
+	console.debug("📨 Received message:", messageWithType);
 
-  switch (messageWithType.type) {
-    case "CONNECT":
-      return onConnectMessage(messageWithType as ConnectMessage);
-    case "DISCONNECT":
-      return onDisconnectMessage();
-    case "CONNECT_STATUS":
-      return onConnectStatusMessage();
-    case "LIST_TRIGGERS":
-      return onListTriggersMessage();
-    case "TRIGGER_EVENT":
-      return onTriggerEventMessage(messageWithType as TriggerEventMessage);
-    case "LOGIN":
-      return onLoginMessage(messageWithType as LoginMessage);
-    case "LOGOUT":
-      return onLogoutMessage();
-    default:
-      return onFallbackMessage();
-  }
+	switch (messageWithType.type) {
+		case "CONNECT":
+			return onConnectMessage(messageWithType as ConnectMessage);
+		case "DISCONNECT":
+			return onDisconnectMessage();
+		case "CONNECT_STATUS":
+			return onConnectStatusMessage();
+		case "LIST_TRIGGERS":
+			return onListTriggersMessage();
+		case "TRIGGER_EVENT":
+			return onTriggerEventMessage(messageWithType as TriggerEventMessage);
+		case "LOGIN":
+			return onLoginMessage(messageWithType as LoginMessage);
+		case "LOGOUT":
+			return onLogoutMessage();
+		default:
+			return onFallbackMessage();
+	}
 };
